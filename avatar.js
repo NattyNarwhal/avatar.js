7fe371b2f55a0d1314e9df9f216f4817c3eec129
Calvin <calvin@openmailbox.org>
2015-03-26 15:52:50 -0300

    simplify if

--- avatar.js
+++ avatar.js
@@ -17,18 +17,14 @@ server.get('/avatar/:hash', function(req, res) {
 		// aliasing var
 		var id = req.params.hash;
 		var p = path.join(avatarpath, id);
-		// the size is needed and gravatar uses 80 default
-		var size = req.query.s ? req.query.s : 80;
-		// if force default, just use that
-		if (req.query.f == "y") {
-			// TODO
-		}
 		// check if even a valid hash (no dir traverse)
 		if (!/^[a-f0-9]+$/.test(id)) {
 			res.sendStatus(400).end(); return;
 		}
-		// if it doesn't exist, fall back to default
-		if (!fs.existsSync(p)) {
+		// the size is needed and gravatar uses 80 default
+		var size = req.query.s ? req.query.s : 80;
+		// if force default OR non-existant, just use default
+		if (req.query.f == "y" || !fs.existsSync(p)) {
 			// TODO
 		}
 		// i guess it's fine
@@ -36,7 +32,7 @@ server.get('/avatar/:hash', function(req, res) {
 			function (err, buffer) {
 				if (err) {
 					console.log("couldn't resize " + id + " to " + size);
-					res.sendStatus(400).end(); return;
+					res.sendStatus(500).end(); return;
 				}
 				res.set("Content-Type", "image/jpg");
 				res.send(buffer).end();
